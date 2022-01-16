f201574bc098154a67db531f13c716f52e083274
Tommi Laukkanen <tlaukkanen@users.noreply.github.com>
2022-01-16 20:45:35 +0200

    Improved layout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
 
 import './App.css';
-import { Button, Grid, IconButton, Table, TableBody, TableCell, TableRow } from '@mui/material';
-import ArrowBack from '@mui/icons-material/ArrowBack';
-import ArrowUpward from '@mui/icons-material/ArrowUpward';
-import ArrowForward from '@mui/icons-material/ArrowForward';
-import ArrowDownward from '@mui/icons-material/ArrowDownward';
-import StopCircle from '@mui/icons-material/StopCircle';
+import { Button, Grid } from '@mui/material';
 import { Joystick } from 'react-joystick-component';
 import PanoramaFishEye from '@mui/icons-material/PanoramaFishEye';
-import PhoneInTalk from '@mui/icons-material/PhoneInTalk';
 import CatchingPokemon from '@mui/icons-material/CatchingPokemon';
 import Close from '@mui/icons-material/Close';
 import Audiotrack from '@mui/icons-material/Audiotrack';
@@ -16,6 +10,9 @@ import BlurOn from '@mui/icons-material/BlurOn';
 import Carpenter from '@mui/icons-material/Carpenter';
 import ChatBubble from '@mui/icons-material/ChatBubble';
 import ContactSupport from '@mui/icons-material/ContactSupport';
+import Looks1 from '@mui/icons-material/LooksOne';
+
+
 import { Box } from '@mui/system';
 
 
@@ -42,8 +39,8 @@ function App() {
         justifyContent="center"
         direction="row"
       >
-        <Grid item xs={12}>
-          <img src="https://source.unsplash.com/random/640x480" alt="background" />
+        <Grid item xs={12} className='halfPanel'>
+          <img src="https://source.unsplash.com/random/640x480" height="100%" alt="background" />
         </Grid>
         <Grid item xs={6}>
           <Box
@@ -52,6 +49,7 @@ function App() {
             alignItems="center"
             justifyContent="center"
             minHeight="200px"
+            className='halfPanel'
           >
             <Joystick
               start={started}
@@ -60,55 +58,65 @@ function App() {
               stop={stopped} />
           </Box>
         </Grid>
-        <Grid item xs={6}>
+        <Grid item xs={6} >
           <Grid container spacing={2} alignItems="center"
             justifyContent="center"
-            direction="row">
-            <Grid item xs={4}>
-              <Button variant="outlined" color="primary">
-                <PhoneInTalk />
-              </Button>
-            </Grid>
-            <Grid item xs={4}>
-              <Button variant="outlined" color="primary">
-                <PanoramaFishEye />
-              </Button>
-            </Grid>
-            <Grid item xs={4}>
-              <Button variant="outlined" color="primary">
-                <Audiotrack />
-              </Button>
-            </Grid>
-            <Grid item xs={4}>
-              <Button variant="outlined" color="primary">
-                <CatchingPokemon />
-              </Button>
-            </Grid>
-            <Grid item xs={4}>
-              <Button variant="outlined" color="primary">
-                <BlurOn />
-              </Button>
-            </Grid>
-            <Grid item xs={4}>
-              <Button variant="outlined" color="primary">
-                <Close />
-              </Button>
-            </Grid>
-            <Grid item xs={4}>
-              <Button variant="outlined" color="primary">
-                <Carpenter />
-              </Button>
-            </Grid>
-            <Grid item xs={4}>
-              <Button variant="outlined" color="primary">
-                <ChatBubble />
-              </Button>
-            </Grid>
-            <Grid item xs={4}>
-              <Button variant="outlined" color="primary">
-                <ContactSupport />
-              </Button>
-            </Grid>
+            direction="row"
+            className='halfPanel'
+          >
+            <Box>
+              <Grid
+                container spacing={2} alignItems="center"
+                justifyContent="center"
+                direction="row"
+              >
+                <Grid item xs={4}>
+                  <Button variant="outlined" color="primary">
+                    <Looks1 />
+                  </Button>
+                </Grid>
+                <Grid item xs={4}>
+                  <Button variant="outlined" color="primary">
+                    <PanoramaFishEye />
+                  </Button>
+                </Grid>
+                <Grid item xs={4}>
+                  <Button variant="outlined" color="primary">
+                    <Audiotrack />
+                  </Button>
+                </Grid>
+                <Grid item xs={4}>
+                  <Button variant="outlined" color="primary">
+                    <CatchingPokemon />
+                  </Button>
+                </Grid>
+                <Grid item xs={4}>
+                  <Button variant="outlined" color="primary">
+                    <BlurOn />
+                  </Button>
+                </Grid>
+                <Grid item xs={4}>
+                  <Button variant="outlined" color="primary">
+                    <Close />
+                  </Button>
+                </Grid>
+                <Grid item xs={4}>
+                  <Button variant="outlined" color="primary">
+                    <Carpenter />
+                  </Button>
+                </Grid>
+                <Grid item xs={4}>
+                  <Button variant="outlined" color="primary">
+                    <ChatBubble />
+                  </Button>
+                </Grid>
+                <Grid item xs={4}>
+                  <Button variant="outlined" color="primary">
+                    <ContactSupport />
+                  </Button>
+                </Grid>
+              </Grid>
+            </Box>
           </Grid>
         </Grid>
       </Grid>
